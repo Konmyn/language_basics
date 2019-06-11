36a259ba77da44ec33138164646c296134196f18
ethan <konmyn@gmail.com>
2019-06-11 09:51:08 +0800

    add script to run etcd service

---
+++ python/etcd/run-etcd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker run -p 2379:2379 --rm -it -p 4001:4001 --name etcd \
+	harbor.geniusafc.com/k8s.gcr.io/etcd:3.2.24 \
+	etcd --listen-client-urls 'http://0.0.0.0:2379' --advertise-client-urls 'http://0.0.0.0:2379'
+
