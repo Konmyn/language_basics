2c934f1662909ffbec83c39ae6ba556657cb259d
ethan <konmyn@gmail.com>
2019-07-04 11:17:19 +0800

    mv out key-record as a dependent project

--- python/key-record/keyrec/keyin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KeyinConfig(AppConfig):
-    name = 'keyin'
--- python/key-record/keyrec/keyin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.keyin, name='index'),
-    path('info/', views.info, name='info'),
-    path('cloud/', views.cloud, name='cloud'),
-]
