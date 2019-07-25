1ab232cc970c3d47e8a21515ce6c646aa6dfc10b
ethan <konmyn@gmail.com>
2019-07-25 14:35:25 +0800

    misspell restructured, open source like

--- python/spell-check/spell-check.py
+++ python/spell-check/spell-check.py
@@ -1,92 +1,80 @@
 import os
 import re
+from collections import defaultdict
 
 from spellchecker import SpellChecker
 
-spell = SpellChecker()
-ext_cache = set()
-uncache = set()
+cache = dict()
 
-regex = r'\b\w+\b'
-pt_url = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
-pt_note = r"@\w+"
-pt_num = r"#\w+"
-pt_comment = r"(?://[^\n]*|/\*(?:(?!\*/).)*\*/)"
-pt_print = r'\("(.*?)"'
-
-# TODO(madhusudancs)
-# @mindprince
-# "github.com/mindprince/gonvml"
-
-w = list()
-with open("words.txt") as f:
-    for line in f.readlines():
-        if line.startswith("#"):
-            continue
-        w.extend(line.strip().split())
-# 载入非错误单词（手工认证）
-spell.word_frequency.load_words(w)
+pattern_word = r'\b\w+\b'
+pattern_url = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+pattern_note = r"@\w+"  # @yourname
+pattern_num = r"#\w+"   # #yourname
+pattern_comment = r"(?://[^\n]*|/\*(?:(?!\*/).)*\*/)"  # /* comment */ or // comment
+pattern_logging = r'\("(.*?)"'  # ("hello world"
+pattern_camelcase = r'[A-Z]?[a-z]+|[A-Z]+(?=[A-Z]|$)'  # helloWorld
 
 
-def main(code_path=None):
-    if not code_path:
-        return
-    for dir, _, fnames in os.walk(code_path):
-        for f in fnames:
-            fpath = os.path.join(dir, f)
-            # print(fpath)
-            feed(fpath)
+TYPO_WORDS_FILE = "tmp.txt"
+KNOWN_WORDS_FILE = "words.txt"
 
 
-def feed(fpath=None):
-    if not fpath:
-        return
-    # 先跳过包管理目录
-    if "/vendor/" in fpath:
-        return
-    ext = os.path.splitext(fpath)[-1]
-    # 对markdown格式的文件进行检索
-    # if ext == ".md":
-    #     # print(fpath)
-    #     read_md(fpath)
-    if ext == ".go":
-        read_go(fpath)
-
-
-# 扫描markdown文件
-def read_md(file):
-    global uncache
-    with open(file, "r") as f:
-        linenum = 0
-        for l in f.readlines():
-            origins = l
-            linenum += 1
-            # 删除url
-            l = re.sub(pt_url, "", l)
-            # 删除@someone这种格式的人名
-            l = re.sub(pt_note, "", l)
-            # 删除#someone这种格式的人名
-            l = re.sub(pt_num, "", l)
-            # 进行第一次分词
-            words = re.findall(regex, l)
-            nwords = list()
-            for w in words:
-                # 跳过带连字符或者包含数字的词
-                if "_" in w or re.match(r".*\d.*", w):
-                    continue
-                # 对camelcase的词进行二次分词
-                nw = re.findall(r'[A-Z]?[a-z]+|[A-Z]+(?=[A-Z]|$)', w)
-                nwords.extend(nw)
-            unkn = spell.unknown(nwords)
-            if not unkn:
+def init_spell_check():
+    checker = SpellChecker()
+    w = list()
+    with open(KNOWN_WORDS_FILE) as f:
+        for line in f.readlines():
+            if line.startswith("#"):
                 continue
-            # print(file, '@', linenum, '->', *unkn)
-            # print(origins)
-            uncache = uncache.union(unkn)
+            w.extend(line.strip().split())
+    # 载入非错误单词（手工认证）
+    checker.word_frequency.load_words(w)
+    return checker
 
 
-# 扫描go文件
-def read_go(file):
+spell = init_spell_check()
+
+
+def run_spell_check(project=None, ext=(".go", ".md")):
+    if not project:
+        return
+    for file in walk_dir(project):
+        if get_file_extension(file) in ext:
+            feed(file)
+
+
+def run_file_ext_statistics(project=None):
+    if not project:
+        return
+    statistics = defaultdict(int)
+    for file in walk_dir(project):
+        if not is_qualified_file(file):
+            continue
+        ext = get_file_extension(file)
+        statistics[ext] += 1
+    stat = [(k, v) for k, v in statistics.items()]
+    stat = sorted(stat, key=lambda i: i[1], reverse=True)
+    for i in stat:
+        print("{:>20s}: {:<4d}".format(i[0], i[1]))
+
+
+def walk_dir(project):
+    for dir, _, fnames in os.walk(project):
+        for f in fnames:
+            file = os.path.join(dir, f)
+            yield file
+
+
+def is_qualified_file(file):
+    if not file:
+        return False
+    # go项目，跳过go的包管理目录
+    if "/vendor/" in file:
+        return False
+    # 跳过 .git 目录
+    if "/.git/" in file:
+        return False
+    # kubernetes项目
     for i in [
         "kubectl/explain/formatter_test.go",
         "kubectl/explain/model_printer_test.go",
@@ -94,73 +82,87 @@ def read_go(file):
         "/generated/",
     ]:
         if i in file:
-            return
-    global uncache
+            return False
+    return True
+
+
+def get_file_extension(file):
+    return os.path.splitext(file)[-1]
+
+
+def feed(file=None):
+    if not is_qualified_file(file):
+        return
+    parse_misspelled(file)
+
+
+def get_text(file):
     with open(file, "r") as f:
-        t = f.read()
-    # comments = re.findall(pt_comment, t, re.DOTALL)
-    comments = re.findall(pt_print, t, re.DOTALL)
-    comments = " ".join(comments)
+        text = f.read()
+    if get_file_extension(file) == ".go":
+        comments = re.findall(pattern_comment, text, re.DOTALL)
+        logs = re.findall(pattern_logging, text, re.DOTALL)
+        text = " ".join(comments) + " " + " ".join(logs)
+    return text
+
+
+def parse_words(string):
+    string = string.replace('\n', ' ').replace('\r', '')
     # 删除url
-    comments = re.sub(pt_url, "", comments)
+    string = re.sub(pattern_url, "", string)
     # 删除@someone这种格式的人名
-    comments = re.sub(pt_note, "", comments)
+    string = re.sub(pattern_note, "", string)
     # 删除#someone这种格式的人名
-    comments = re.sub(pt_num, "", comments)
-    words = re.findall(regex, comments)
-    nwords = list()
-    for w in words:
+    string = re.sub(pattern_num, "", string)
+    # 进行第一次分词
+    raw_words = re.findall(pattern_word, string)
+    words = list()
+    for w in raw_words:
         # 跳过带连字符或者包含数字的词
         if "_" in w or re.match(r".*\d.*", w):
             continue
+        if len(w) <= 2:
+            continue
         # 对camelcase的词进行二次分词
-        nw = re.findall(r'[A-Z]?[a-z]+|[A-Z]+(?=[A-Z]|$)', w)
-        nwords.extend(nw)
-    unkn = spell.unknown(nwords)
-    if not unkn:
+        word_list = re.findall(pattern_camelcase, w)
+        words.extend(word_list)
+    return words
+
+
+def parse_misspelled(file):
+    raw_text = get_text(file)
+    words = parse_words(raw_text)
+    bad_words = spell.unknown(words)
+    if not bad_words:
         return
-    print(file, unkn)
-    uncache = uncache.union(unkn)
-    comment_cache(file, unkn)
+    update_cache(file, bad_words)
 
 
-ccache = dict()
+def update_cache(file, bad_words):
+    global cache
+    cache[file[27:]] = bad_words
 
 
-def comment_cache(file, nwords):
-    global ccache
-    l = len(nwords)
-    if l not in ccache:
-        ccache[l] = [1, [file]]
-    else:
-        ccache[l][0] += 1
-        ccache[l][1].append(file)
+def print_cache():
+    global cache
+    output = defaultdict(list)
+    for k, v in cache.items():
+        output[len(v)].append((k, v))
+    lens = sorted(list(output.keys()), reverse=True)
+    for i in lens:
+        print("#", i)
+        for j in output[i]:
+            print(j[0], "->", j[1])
 
 
-def print_ccache():
-    global ccache
-    l = list(ccache.keys())
-    l.sort()
-    for i in reversed(l):
-        print(i)
-        print(ccache[i])
-
-
-if __name__ == "__main__":
-    # code_path = "/home/matrix/workspace/github/helm"
-    code_path = "/home/matrix/go/src/k8s.io/kubernetes"
-    # code_path = "/home/matrix/go/src/github.com/prometheus/prometheus"
-    # code_path = "/home/matrix/workspace/github/envoy"
-    # code_path = "/home/matrix/workspace/github/fluentd"
-    # code_path = "/home/matrix/workspace/github/coredns"
-    # code_path = "/home/matrix/workspace/github/containerd"
-    # code_path = "/home/matrix/workspace/github/tikv"
-    # code_path = "/home/matrix/workspace/github/etcd"
-    main(code_path)
-    # 把找到的所有错误单词存入临时文件
-    result = list(uncache)
+def save_typo_words():
+    global cache
+    bad_words = set()
+    for v in cache.values():
+        bad_words = bad_words.union(v)
+    result = list(bad_words)
     result.sort()
-    with open("tmp.txt", "w") as t:
+    with open(TYPO_WORDS_FILE, "w") as t:
         l = list()
         for i in result:
             l.append(i)
@@ -170,4 +172,21 @@ if __name__ == "__main__":
                 continue
         else:
             t.write(" ".join(l) + "\n")
-    print_ccache()
+
+
+def get_projects(project="kubernetes"):
+    if project == "kubernetes":
+        return "/home/matrix/go/src/k8s.io/kubernetes"
+    elif project == "prometheus":
+        return "/home/matrix/go/src/github.com/prometheus/prometheus"
+    else:
+        # helm envoy fluentd coredns containerd tikv etcd
+        return os.path.join("/home/matrix/workspace/github", project)
+
+
+if __name__ == "__main__":
+    project = get_projects()
+    # run_file_ext_statistics(project=project)
+    run_spell_check(project=project)
+    save_typo_words()
+    print_cache()
