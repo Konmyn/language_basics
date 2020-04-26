3d0c3cb517e3474f837ec3634910305be004bf49
Guangming Wang <guangming.wang@daocloud.io>
2020-04-26 09:52:17 +0800

    delete unused data

--- python/cephconn/build-push.sh
+++ python/cephconn/build-push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t harbor.geniusafc.com/restrict/harbor-registry-migration:python3-stretch .
-docker push harbor.geniusafc.com/restrict/harbor-registry-migration:python3-stretch
+docker build -t docker.io/restrict/harbor-registry-migration:python3-stretch .
+docker push docker.io/restrict/harbor-registry-migration:python3-stretch
--- python/etcd/run-etcd.sh
+++ python/etcd/run-etcd.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker run -p 2379:2379 --rm -it -p 4001:4001 --name etcd \
-	harbor.geniusafc.com/k8s.gcr.io/etcd:3.2.24 \
+	docker.io/k8s.gcr.io/etcd:3.2.24 \
 	etcd --listen-client-urls 'http://0.0.0.0:2379' --advertise-client-urls 'http://0.0.0.0:2379'
-
